e83645fee3c2e550d6d4d454a2b99bd80c572fd2
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-23 09:42:17 +0600

    banner design

--- src/pages/Homepage/Banner/Banner.jsx
+++ src/pages/Homepage/Banner/Banner.jsx
@@ -1,35 +1,63 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import banner1 from '../../../assets/banner1.jpg'
-import banner2 from '../../../assets/banner2.jpg'
-import banner3 from '../../../assets/banner3.jpg'
-import banner4 from '../../../assets/banner4.jpg'
+import banner8 from '../../../assets/banner8.jpg'
+import banner5 from '../../../assets/banner5.jpg'
+import banner6 from '../../../assets/banner6.jpg'
+import banner7 from '../../../assets/banner7.jpg'
 const Banner = () => {
     
     return (
+
+
         <div>
-               <Carousel   autoPlay
-                interval={3000}
-                transitionTime={500} // Set the transition duration in milliseconds
-                showThumbs={false} // Hide the thumbnail navigation
-                showIndicators={false} // Hide the indicator dots
-                infiniteLoop // Enable infinite loop
+               <Carousel   
+               autoPlay
+                interval={4000}
+                transitionTime={1000}
+                infiniteLoop
+                // showStatus={false}
+        showThumbs={false}
+               
                >
-                <div style={{height: '100vh'}} className="hero">
-                    <img style={{ width: '100%', height: '100%' }} src={banner1} />
-                    <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero" style={{ height: '100vh', backgroundImage: `url("${banner7}")` }}>
+                     <div className="hero-overlay bg-opacity-60 bg-black"></div>
+                     <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md text-white">
+      <h1 className="mb-5 text-6xl font-thin font-serif">Good Food</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
                 </div>
-                <div style={{height: '100vh'}} className="hero">
-                    <img style={{ width: '100%', height: '100%' }} src={banner2} />
-                    <div className="hero-overlay bg-opacity-30"></div>
+                <div className="hero" style={{ height: '100vh', backgroundImage: `url("${banner5}")` }}>
+                     <div className="hero-overlay bg-opacity-60 bg-black"></div>
+                     <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md text-white">
+      <h1 className="mb-5 text-6xl font-thin font-serif">Good Environment</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
                 </div>
-                <div style={{height: '100vh'}} className="hero">
-                    <img style={{ width: '100%', height: '100%' }} src={banner3} />
-                    <div className="hero-overlay bg-opacity-30"></div>
+                <div className="hero" style={{ height: '100vh', backgroundImage: `url("${banner6}")` }}>
+                     <div className="hero-overlay bg-opacity-60 bg-black"></div>
+                     <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md text-white">
+      <h1 className="mb-5 text-6xl font-thin font-serif">Good Services</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
                 </div>
-                <div style={{height: '100vh'}} className="hero">
-                    <img style={{ width: '100%', height: '100%' }} src={banner4} />
-                    <div className="hero-overlay bg-opacity-30"></div>
+                <div className="hero" style={{ height: '100vh', backgroundImage: `url("${banner8}")` }}>
+                     <div className="hero-overlay bg-opacity-60 bg-black"></div>
+                     <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md text-white">
+      <h1 className="mb-5 text-6xl font-thin font-serif">Good Care</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
                 </div>
             </Carousel>
         </div>
