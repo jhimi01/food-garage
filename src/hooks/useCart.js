911db4ffddbc4e3f44f9c90b8481ee51058265e2
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-03 15:57:10 +0600

    dfds

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,20 +1,26 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../provider/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './useAuth';
 
 const useCart = () => {
-  const { user } = useContext(AuthContext)
-  const token = localStorage.getItem('access-token')
+  const { user } = useAuth()
+  // const token = localStorage.getItem('access-token')
+  const [axiosSecure] = useAxiosSecure()
 
   const { refetch,  data: cart = [] } = useQuery({
     queryKey: ['cart', user?.email],
+    // queryFn: async ()=>{
+    //     const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+    //       headers: {
+    //         authorization: `bearer ${token}`
+    //       }
+    //     })
+    //     return response.json()
+    // },
     queryFn: async ()=>{
-        const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
-          headers: {
-            authorization: `bearer ${token}`
-          }
-        })
-        return response.json()
+        const response = await axiosSecure(`/carts?email=${user?.email}`)
+        console.log('res from axios secure', response)
+        return response.data;
     },
   })
    return [cart, refetch]
