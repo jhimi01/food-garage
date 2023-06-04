7c116e18f7cbba5a85849999cbbb4b126ac2bad6
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-04 21:59:25 +0600

    payment key

--- src/pages/Dashboard/Payment/Checkoutform.jsx
+++ src/pages/Dashboard/Payment/Checkoutform.jsx
@@ -1,7 +1,9 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 
 const Checkoutform = () => {
+    const [cardError, setCardError] = useState('')
     const stripe = useStripe()
     const elements = useElements();
 
@@ -18,11 +20,24 @@ const Checkoutform = () => {
           if (card == null) {
             return;
           }
-
-    }
+          console.log(card)
+          const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+          });
+      
+          if (error) {
+            console.log('[error]', error);
+            setCardError(error.message)
+          } else {
+            console.log('[PaymentMethod]', paymentMethod);
+            setCardError('')
+          }
+        };
 
     return (
-        <form onSubmit={handleSubmit}>
+       <>
+         <form onSubmit={handleSubmit}>
         <CardElement
           options={{
             style: {
@@ -39,10 +54,12 @@ const Checkoutform = () => {
             },
           }}
         />
-        <button type="submit" disabled={!stripe}>
+        <button className="btn btn-active" type="submit" disabled={!stripe}>
           Pay
         </button>
       </form>
+      {cardError && <p className="text-red-600">{cardError}</p>}
+       </>
     );
 };
 
